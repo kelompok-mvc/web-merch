ebf2b3bfd2123f65f9fabf16d0fb9d4e55999f78
salman4ll <sfarisi0101@gmail.com>
2024-03-14 13:55:20 +0700

    init project

---
+++ app/views/components/footer.php
@@ -0,0 +1,10 @@
+    <footer>
+        
+    </footer>
+    
+    <script src="<?= BASEURL ?>/vendor/aos/aos.js"></script>
+    <script src="<?= BASEURL ?>/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="<?= BASEURL ?>/js/main.js"></script>
+</body>
+
+</html>
