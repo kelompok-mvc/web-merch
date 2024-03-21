7da32d5f1d8d271a0f955cda6d0a6c195af12c48
syifa sukma <syifasukmaramadhani@gmail.com>
2024-03-21 16:17:28 +0700

    menambahkan tampilan home

--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,1 +1,55 @@
-<h1><?= $data['judul'] ?></h1>
+<div class="mb-2">
+  <div class="row">
+    <div class="col-xl-4 mt-3">
+      <div class="card d-flex flex-column py-3">
+        <div class="row row-0 flex-fill">
+          <div class="col-md-3">
+            <a href="<?= BASEURL; ?>/transaksi">
+              <img src="<?= BASEURL ?>/img/wallet.png" class="w-100 h-100 object-cover" alt="Card side image" />
+            </a>
+          </div>
+          <div class="col">
+            <div class="card-body">
+              <h1 class="text-xxl text-primary"><a href="<?= BASEURL; ?>/transaksi">Transaksi</a></h1>
+              <p>Untuk melakukan transaksi </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-4 mt-3">
+      <div class="card py-3">
+        <div class="row row-0">
+          <div class="col-sm-3">
+            <a href="<?= BASEURL; ?>/customer">
+              <img src="<?= BASEURL ?>/img/customers.png" class="w-100 h-100 object-cover" alt="Card side image" />
+            </a>
+          </div>
+          <div class="col">
+            <div class="card-body">
+              <h1 class="text-xxl text-primary"><a href="<?= BASEURL; ?>/customer">Customers</a></h1>
+              <p>Untuk melihat seluruh data customer</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="col-xl-4 mt-3">
+      <div class="card d-flex flex-column py-3">
+        <div class="row row-0 flex-fill">
+          <div class="col-md-3">
+            <a href="<?= BASEURL; ?>/laporan">
+              <img src="<?= BASEURL ?>/img/report.png" class="w-100 h-100 object-cover" alt="Card side image" />
+            </a>
+          </div>
+          <div class="col">
+            <div class="card-body">
+              <h1 class="text-xxl text-primary"><a href="<?= BASEURL; ?>/laporan">Laporan Penjualan</a></h1>
+              <p>Untuk melihat seluruh data penjualan</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
